aa09c2924e30c088de88e7a361a9dd807e82c03c
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-07-28 09:13:52 +0530

    File updated at 7/28/2026, 9:13:52 AM

--- 292-NimGame/292-NimGame.cpp
+++ 292-NimGame/292-NimGame.cpp
@@ -1,7 +1,7 @@
-// Last updated: 4/6/2026, 5:56:37 PM
-class Solution {
-public:
-    bool canWinNim(int n) {
-        return n%4 !=0;
-    }
-};
+// Last updated: 7/28/2026, 9:13:52 AM
+1class Solution {
+2public:
+3    bool canWinNim(int n) {
+4         return n%4 !=0;
+5    }
+6};
