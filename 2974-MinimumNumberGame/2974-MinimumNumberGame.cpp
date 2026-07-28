1d4172bab14303d7cbc5a5ea94a15d02e246cd35
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-07-28 15:33:38 +0530

    File updated at 7/28/2026, 3:33:37 PM

--- 2974-MinimumNumberGame/2974-MinimumNumberGame.cpp
+++ 2974-MinimumNumberGame/2974-MinimumNumberGame.cpp
@@ -1,15 +1,20 @@
-// Last updated: 7/28/2026, 3:03:55 PM
+// Last updated: 7/28/2026, 3:33:37 PM
 1class Solution {
 2public:
-3    vector<int> numberGame(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
-5        
-6        vector<int> arr;
-7        
-8        for(int i=0; i<nums.size(); i+=2){
-9            arr.push_back(nums[i+1]);
-10            arr.push_back(nums[i]);
-11        }
-12        return arr;
-13    }
-14};  
+3    int differenceOfSum(vector<int>& nums) {
+4        int Esum=0;
+5        int Dsum=0;
+6        for(int i=0; i<nums.size(); i++){
+7            Esum += nums[i];
+8            if(nums[i]>9){
+9                while(nums[i] != 0){
+10                    Dsum += nums[i]%10;
+11                    nums[i]/=10;
+12                }
+13                continue;
+14            }
+15            Dsum += nums[i];
+16        }
+17        return abs(Dsum - Esum);
+18    }
+19};
