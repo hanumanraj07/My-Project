d5e7051a2be50ee66f4d20d8a3d6016b0bbe4bf6
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-07-30 15:56:31 +0530

    File updated at 7/30/2026, 3:56:31 PM

--- 443-StringCompression/443-StringCompression.cpp
+++ 443-StringCompression/443-StringCompression.cpp
@@ -1,21 +1,8 @@
-// Last updated: 7/30/2026, 3:27:26 PM
+// Last updated: 7/30/2026, 3:56:31 PM
 1class Solution {
 2public:
-3    string compressedString(string word) {
-4        string comp = "";
-5        int cnt = 1, n = word.size();
-6        char ch = word[0];
-7        for(int i=1;i<n;i++){
-8            if(word[i] == ch && cnt < 9)cnt++;
-9            else{
-10                comp.push_back(cnt+'0');
-11                comp.push_back(ch);
-12                ch = word[i];
-13                cnt = 1;
-14            }
-15        }
-16        comp.push_back(cnt+'0');
-17        comp.push_back(ch);
-18        return comp;
-19    }
-20};
+3    int minimumPushes(string A) {
+4        auto q = A.size() >> 3, r = A.size() & 7;
+5        return ((q << 2) + r) * (q + 1);
+6    }
+7};
