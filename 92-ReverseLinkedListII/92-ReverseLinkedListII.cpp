d88d5d07a355d7df6abf1455648cd14889d5a995
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-07 16:57:53 +0530

    File updated at 8/7/2026, 4:57:51 PM

--- 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
+++ 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/7/2026, 3:50:23 PM
+// Last updated: 8/7/2026, 4:57:51 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -11,30 +11,55 @@
 10 */
 11class Solution {
 12public:
-13    ListNode* rotateRight(ListNode* head, int k) {
-14        if (!head || !head->next || k == 0) return head;
-15 
-16        int n = 1;
-17        ListNode* tail = head;
-18        while (tail->next) {
-19            tail = tail->next;
-20            n++;
-21        }
-22 
-23        k = k % n;
-24        if (k == 0) return head;
-25 
-26        tail->next = head;
-27 
-28        int steps = n - k;
-29        ListNode* newTail = head;
-30        while (--steps) {
-31            newTail = newTail->next;
-32        }
-33 
-34        ListNode* newHead = newTail->next;
-35        newTail->next = nullptr;
-36
-37        return newHead;
-38    }
-39};
+13    ListNode* reverseList(ListNode* head)
+14    {
+15        ListNode* prev = NULL;
+16        ListNode* cur = head;
+17        ListNode* next = NULL;
+18
+19        while(cur)
+20        {
+21            next = cur->next;
+22            cur->next = prev;
+23            prev = cur;
+24            cur = next;
+25        }
+26
+27        return prev;
+28    }
+29
+30    ListNode* swapPairs(ListNode* head) {
+31        if(!head || !head->next) return head;
+32
+33        ListNode* temp = head;
+34        ListNode* prev = NULL;
+35        while(temp)
+36        {
+37            ListNode* start = temp;
+38
+39            for(int i = 0 ; i<1 && temp; i++)
+40            {
+41                temp = temp->next;
+42            }
+43
+44            if(!temp)
+45            {
+46                if(prev) prev->next = start;
+47                break;
+48            }
+49
+50            ListNode* nextNode = temp->next;
+51            temp->next = NULL;
+52
+53            ListNode* newHead = reverseList(start);
+54
+55            if(start==head) head = newHead;
+56            else prev->next = newHead;
+57
+58            prev = start;
+59            temp = nextNode;
+60        }
+61        
+62        return head;
+63    }
+64};
