996ef7b095e3d27b9d27f14860ab3cf621d6033f
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:48:38 +0530

    Create 328-OddEvenLinkedList

---
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/25/2026, 11:48:37 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* temp=head->next;
+        while(odd && odd->next && even && even->next) {
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=temp;
+        return head;
+    }
+};
