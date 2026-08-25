3db17ac7edabc72e44fd434f81debceafda6bc71
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:42:31 +0530

    Create 3206-FindCommonElementsBetweenTwoArrays

---
+++ 3206-FindCommonElementsBetweenTwoArrays/3206-FindCommonElementsBetweenTwoArrays.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/25/2026, 11:42:30 AM
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+
+        int answer1 = 0, answer2 = 0;
+
+        for (int x : nums1) {
+            if (set2.count(x)) {
+                answer1++;
+            }
+        }
+
+        for (int x : nums2) {
+            if (set1.count(x)) {
+                answer2++;
+            }
+        }
+
+        return {answer1, answer2};
+    }
+};
