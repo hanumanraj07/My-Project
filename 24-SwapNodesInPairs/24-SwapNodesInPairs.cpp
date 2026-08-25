46665f783a7389a3e3f2ada27841ac7358c51919
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:51:26 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.cpp
@@ -0,0 +1,65 @@
+// Last updated: 8/25/2026, 11:51:25 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* prev = NULL;
+        ListNode* cur = head;
+        ListNode* next = NULL;
+
+        while(cur)
+        {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp)
+        {
+            ListNode* start = temp;
+
+            for(int i = 0 ; i<1 && temp; i++)
+            {
+                temp = temp->next;
+            }
+
+            if(!temp)
+            {
+                if(prev) prev->next = start;
+                break;
+            }
+
+            ListNode* nextNode = temp->next;
+            temp->next = NULL;
+
+            ListNode* newHead = reverseList(start);
+
+            if(start==head) head = newHead;
+            else prev->next = newHead;
+
+            prev = start;
+            temp = nextNode;
+        }
+        
+        return head;
+    }
+};
