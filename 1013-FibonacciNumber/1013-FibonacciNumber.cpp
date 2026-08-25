827be196276087890798ea459980ae2a7a7bbd6c
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:47:06 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/25/2026, 11:47:05 AM
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        int a = 0 , b = 1;
+
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
