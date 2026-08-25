46fb72b3340e7385c45e5b7f09dd6181cc421f42
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:42:21 +0530

    Create 3321-TypeOfTriangle

---
+++ 3321-TypeOfTriangle/3321-TypeOfTriangle.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/25/2026, 11:42:20 AM
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        set <int> s(nums.begin(), nums.end());
+
+        sort(nums.begin(), nums.end());
+
+        if((nums[0]+ nums[1]) <= nums[2]){
+            return "none";
+        }
+
+        if(s.size() == 1)
+        return "equilateral";
+
+        if(s.size() == 2)
+        return "isosceles";
+
+        return "scalene";
+
+         }
+};
