0c27576d1bf8b387854f2547577040c6e94c3001
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:44:27 +0530

    Create 2122-CountSpecialQuadruplets

---
+++ 2122-CountSpecialQuadruplets/2122-CountSpecialQuadruplets.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/25/2026, 11:44:27 AM
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+         unordered_map<int, int> ump;
+        int count = 0, n = nums.size();
+        for (int c = 2; c < n-1; c++) {
+            int b = c - 1;
+            for (int a = 0; a < b; a++) {
+                ump[nums[a]+nums[b]]++;
+            }
+            for (int d = c+1; d < n; d++) {
+                count += ump[nums[d]-nums[c]];
+            }
+        }
+        return count;
+    }
+};
