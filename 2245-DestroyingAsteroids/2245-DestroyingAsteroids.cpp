7ea40379d9f09ceb8fad3772eff473e81e6b24de
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-08-25 11:44:04 +0530

    Create 2245-DestroyingAsteroids

---
+++ 2245-DestroyingAsteroids/2245-DestroyingAsteroids.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/25/2026, 11:44:03 AM
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+
+        long long currMass = mass;
+
+        for(int asteroid : asteroids){
+            if(currMass < asteroid)
+                return false;
+
+                currMass += asteroid;
+            
+        }
+            return true;
+    }
+};
